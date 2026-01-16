80379580e97ec35107c60bdbbdc21d4982b9dcb7
MaxanceF <147034225+MaxanceF@users.noreply.github.com>
2026-01-16 11:34:21 +0100

    First push + Anim

---
+++ ShaderProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,117 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] InputActionProperty _move;
+    [SerializeField] InputActionProperty _sprint;
+    [SerializeField] float _speed = 5;
+    [SerializeField] CharacterController _characterController;
+    [SerializeField] bool _HasGravity = true;
+    [SerializeField] Animator _animator;
+
+    Vector3 _direction;
+    private Vector3 velocity = Vector3.zero;
+
+    private Vector3 finalDirection = Vector3.zero;
+
+    private Vector2 lastMousePos;
+    private float mouseStillTimer = 0f;
+    private const float mouseStillThreshold = 0.05f;
+    private const float mouseStillDelay = 0.1f;
+
+    void Start()
+    {
+        _move.action.Enable();
+        _move.action.performed += StartMove;
+        _move.action.canceled += StopMove;
+        _sprint.action.performed += StartSprint;
+        _sprint.action.canceled += StopSprint;
+    }
+
+    void OnDestroy()
+    {
+        _move.action.performed -= StartMove;
+        _move.action.canceled -= StopMove;
+        _sprint.action.performed -= StartSprint;
+        _sprint.action.canceled -= StopSprint;
+    }
+
+    void StartMove(InputAction.CallbackContext ctx)
+    {
+        if (_animator.GetBool("IsRunning"))
+        {
+            _speed = 8;
+        }
+        else
+        {
+            _speed = 5;
+        }
+        var d = ctx.ReadValue<Vector2>();
+        _direction = new Vector3(d.x, 0, d.y);
+    }
+
+    void StopMove(InputAction.CallbackContext ctx)
+    {
+        _direction = Vector3.zero;
+    }
+
+    void Update()
+    {
+        UpdateMove();
+        RotateTowardsInput();
+    }
+    
+    void StartSprint(InputAction.CallbackContext ctx)
+    {
+        _animator.SetBool("IsIdle", false);
+        _animator.SetBool("IsWalking", false);
+        _animator.SetBool("IsRunning", true);
+    }
+    void StopSprint(InputAction.CallbackContext ctx)
+    {
+        _animator.SetBool("IsIdle", true);
+        _animator.SetBool("IsWalking", true);
+        _animator.SetBool("IsRunning", false);
+    }
+
+    public void TpTo(Vector3 TPpos)
+    {
+        _characterController.enabled= false;
+        transform.position = TPpos;
+        _characterController.enabled = true;
+    }
+    void UpdateMove()
+    {
+        finalDirection = _direction;
+        if (Camera.main != null)
+        {
+            var forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
+            var right = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+
+            finalDirection = (forward * _direction.z + right * _direction.x).normalized;
+        }
+
+        if (_characterController.isGrounded)
+            velocity.y = -1.0f;
+        else if (_HasGravity)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        Vector3 move = finalDirection * _speed;
+        move += velocity;
+        _characterController.Move(move * Time.deltaTime);
+    }
+    
+    void RotateTowardsInput()
+    {
+        if (finalDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(finalDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 15f * Time.deltaTime);
+        }
+    }
+}
